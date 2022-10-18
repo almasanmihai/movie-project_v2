00716b73f1ca8bf3370463d6228fd12e95364214
almasanmihai <almasanmihai@yahoo.com>
2022-10-18 12:44:44 +0300

    Centered cards in html

--- main.py
+++ main.py
@@ -259,5 +259,5 @@ def select():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port='5000')
-    # app.run(debug=True)
+    # app.run(host="0.0.0.0", port='5000')
+    app.run(debug=True)
